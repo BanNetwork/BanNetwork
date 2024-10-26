3cf1a298319a33e1a7b4a69de6650b0395653186
la_patate_chaude <lusrox28908@gmail.com>
2024-10-26 21:36:25 +0200

    .

---
+++ src/main/java/com/site999/bannetwork/Bukkit/BanNetworkSpigot.java
@@ -0,0 +1,10 @@
+package com.site999.bannetwork.Bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BanNetworkSpigot extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
